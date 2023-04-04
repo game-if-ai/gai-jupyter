14bbad811d21db1a1c538981f9624e3c32abc6c1
aaronshiel <ashiel408@gmail.com>
2023-04-03 23:25:26 -0700

    add hints

--- client/src/components/Output.tsx
+++ client/src/components/Output.tsx
@@ -5,7 +5,6 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 import { IOutput, isError, isStream } from "@jupyterlab/nbformat";
-import { OutputRenderer } from "@datalayer/jupyter-react/lib/components/output/OutputRenderer";
 import { JsonView, defaultStyles } from "react-json-view-lite";
 import "react-json-view-lite/dist/index.css";
 import { splitListOfStringsBy } from "../utils";
